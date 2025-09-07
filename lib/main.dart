7618eaeb388a48a759389449906c1649ad1ac65e
rafa18pe <129650813+rafa18pe@users.noreply.github.com>
2025-09-07 09:38:24 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,50 @@
-
 import 'package:flutter/material.dart';
 import 'package:jewelcraft_ai/presentation/ai_text_to_jewelry/ai_text_to_jewelry_screen.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'JewelCraft AI',
-                style: TextStyle(fontSize: 32, color: Colors.white),
-              ),
-              SizedBox(height: 24),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => AiTextToJewelryScreen(),
-                    ),
-                  );
-                },
-                child: Text('Texto → Joya'),
-              ),
-            ],
-          ),
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'JewelCraft AI',
+              style: TextStyle(fontSize: 32, color: Colors.white),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const AiTextToJewelryScreen(),
+                  ),
+                );
+              },
+              child: const Text('Texto → Joya'),
+            ),
+          ],
         ),
-        backgroundColor: Colors.black,
       ),
-    ),
-  );
+    );
+  }
 }
