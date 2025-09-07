da3ec7879039295f77f1e57424424e0d93b10920
rafa18pe <rafape18@gmail.com>
2025-09-07 14:17:52 +0200

    Añado selector de metal y uso el elegido

--- lib/presentation/ai_text_to_jewelry/ai_text_to_jewelry_screen.dart
+++ lib/presentation/ai_text_to_jewelry/ai_text_to_jewelry_screen.dart
@@ -2,9 +2,16 @@ import 'package:jewelcraft_ai/presentation/preview/preview_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:jewelcraft_ai/services/ai_text_to_stl_service.dart';
 import 'package:jewelcraft_ai/services/precio_calculator.dart';
+import 'package:jewelcraft_ai/models/tipo_metal.dart';
 
-class AiTextToJewelryScreen extends StatelessWidget {
+class AiTextToJewelryScreen extends StatefulWidget {
+  @override
+  State<AiTextToJewelryScreen> createState() => _AiTextToJewelryScreenState();
+}
+
+class _AiTextToJewelryScreenState extends State<AiTextToJewelryScreen> {
   final _controller = TextEditingController();
+  TipoMetal _selectedMetal = TipoMetal.oro18k;
 
   @override
   Widget build(BuildContext context) {
@@ -18,18 +25,30 @@ class AiTextToJewelryScreen extends StatelessWidget {
               controller: _controller,
               decoration: const InputDecoration(labelText: 'Describe tu joya'),
             ),
+            const SizedBox(height: 12),
+            DropdownButton<TipoMetal>(
+              value: _selectedMetal,
+              items: TipoMetal.values.map((m) {
+                return DropdownMenuItem(
+                  value: m,
+                  child: Text(m.name.toUpperCase()),
+                );
+              }).toList(),
+              onChanged: (m) => setState(() => _selectedMetal = m!,
+            ),
             const SizedBox(height: 24),
             ElevatedButton(
               onPressed: () async {
                 final (stlFile, price) = await AiTextToStlService.textToJewelry(
                   _controller.text,
-                  TipoMetal.oro18k,
-                  PrecioCalculator.densidadOro18k,
-                );
+               final (stlFile, price) = await AiTextToStlService.textToJewelry(
+                  _controller.text,
+                  _selectedMetal,
+);                );
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (_) => PreviewScreen(stlFile: stlFile.path, priceEUR: price, metal: TipoMetal.oro18k),
+                    builder: (_) => PreviewScreen(stlFile: stlFile.path, priceEUR: price, metal: _selectedMetal,),
                   ),
                 );
               },
