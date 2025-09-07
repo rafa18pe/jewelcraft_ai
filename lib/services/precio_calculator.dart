5288c0fc4071f5f52b8eea51f1ef14a566a17e94
rafa18pe <rafape18@gmail.com>
2025-09-07 13:30:34 +0200

    Arreglo precio_calculator con todos los tipos de metal

--- lib/services/precio_calculator.dart
+++ lib/services/precio_calculator.dart
@@ -3,20 +3,40 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'dart:typed_data';
 import 'package:jewelcraft_ai/core/api_keys.dart';
-
-enum TipoMetal { oro18k, oro24k, plata950 }
+import 'package:jewelcraft_ai/models/tipo_metal.dart';
 
 class PrecioCalculator {
-  static const double densidadOro18k = 15.6;
-  static const double densidadOro24k = 19.32;
-  static const double densidadPlata950 = 10.1;
-
   static Future<double> calculatePrice(double volumenCm3, TipoMetal metal) async {
-    final densidad = metal == TipoMetal.oro18k ? densidadOro18k : densidadOro24k;
+    // densidad según el metal
+    final densidad = switch (metal) {
+      TipoMetal.oro18k   => 15.6,
+      TipoMetal.oro16k   => 14.9,
+      TipoMetal.oro14k   => 14.0,
+      TipoMetal.oro10k   => 11.6,
+      TipoMetal.oro8k    => 10.4,
+      TipoMetal.plata975 => 10.2,
+      TipoMetal.plata950 => 10.1,
+    };
+
     final pesoGramos = volumenCm3 * densidad;
+
+    // símbolo para la API
+    final symbol = switch (metal) {
+      TipoMetal.oro18k ||
+      TipoMetal.oro16k ||
+      TipoMetal.oro14k ||
+      TipoMetal.oro10k ||
+      TipoMetal.oro8k   => 'XAU',
+      TipoMetal.plata975 ||
+      TipoMetal.plata950 => 'XAG',
+    };
+
     final dio = Dio();
     final response = await dio.get(
-    'https://api.alltick.co/quote?symbol=${metal == TipoMetal.oro18k ? 'XAU' : 'XAG'}-EUR&api_token=${await ApiKeys.alltick}'    final precioEURPorGramo = double.parse(response.data['price'].toString()) / 31.1035;
+      'https://api.alltick.co/quote?symbol=$symbol-EUR&api_token=${await ApiKeys.alltick}',
+    );
+
+    final precioEURPorGramo = double.parse(response.data['price'].toString()) / 31.1035;
     return pesoGramos * precioEURPorGramo;
   }
 }
