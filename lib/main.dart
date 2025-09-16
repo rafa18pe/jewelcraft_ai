8302bc5c5af8682d0f4a0d40cfb34204ba885b96
rafa18pe <129650813+rafa18pe@users.noreply.github.com>
2025-09-17 00:31:22 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:jewelcraft_ai/presentation/ai_text_to_jewelry/ai_text_to_jewelry_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+ // Logger interno: guarda en la carpeta privada de la app
+final dir = await getApplicationDocumentsDirectory();
+final logFile = File('${dir.path}/jewelcraft_log.txt');
+try {
+  logFile.writeAsStringSync('APP INICIADA\n');
+  // Aquí puedes ir añadiendo más logs
+} catch (e) {
+  logFile.writeAsStringSync('ERROR EN MAIN: $e\n', mode: FileMode.append);
+}
   await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
